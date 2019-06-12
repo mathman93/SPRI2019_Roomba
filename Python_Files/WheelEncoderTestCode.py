3854f746be449ce69187d11c1788a1527a806963
Sbuckley19 <51656135+Sbuckley19@users.noreply.github.com>
2019-06-12 14:16:43 -0400

    Update WheelEncoderTestCode.py

--- Python_Files/WheelEncoderTestCode.py
+++ Python_Files/WheelEncoderTestCode.py
@@ -63,16 +63,23 @@ file = open(file_name, "w") # Open a text file for storing data
 start_time = time.time()
 
 
-dict = {0:[100,0,10],
+dict = {0:[100,100,10],
 	1:[0,100,5],
-	2:[50,50,5]}
+	2:[50,50,5]
+	3:[20,40,3]
+	4:[45,10,3]
+	5:[100,0,3]}
 [left_start,right_start]=Roomba.Query(43,44)
 
 
 y_position = 0
 x_position = 0
 theta = 0
-counter=0
+wheel_diameter = 72
+counts_per_rev = 508.8
+distance_between_wheels = 235
+C_theta = (wheel_diameter*math.pi)/(counts_per_rev*distance_between_wheels)
+distance_per_count = (wheel_diameter*math.pi)/counts_per_rev
 data_time = time.time()
 file.write("{0},{1},{2},{3},{4},{5}\n".format(0,left_start, right_start,x_position,y_position,theta))
 Roomba.StartQueryStream(43,44)
@@ -84,13 +91,12 @@ for i in range(len(dict.keys())):
 		if Roomba.Available()>0:
 			data_time2 = time.time()
 			[left_encoder, right_encoder]=Roomba.ReadQueryStream(43,44)
-			counter += 1
 			delta_l = left_encoder-left_start
 			delta_r = right_encoder-right_start
-			delta_theta = (delta_l-delta_r)*((72*math.pi)/(508.8*235))
+			delta_theta = (delta_l-delta_r)*C_theta
 			theta += delta_theta
 			if delta_l-delta_r == 0:
-				delta_d = 0.5*(delta_l+delta_r)*((72*math.pi)/508.8)
+				delta_d = 0.5*(delta_l+delta_r)*distance_per_count
 			else:
 				delta_d = 2*(235*(delta_l/(delta_l-delta_r)-.5))*math.sin(delta_theta/2)
 			
