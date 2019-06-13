86fd41a408290520c7ff23460faf9ada9bf64d11
David Croft <dcroft2001@gmail.com>
2019-06-13 14:55:12 -0400

    Update SpinChange.py

--- Python_Files/SpinChange.py
+++ Python_Files/SpinChange.py
@@ -90,7 +90,7 @@ print("{0:.6f},{1},{2},{3:.3f},{4:.3f},{5:.6f},{6},{7}".format(0,left_start,righ
 
 #file.write("{0},{1},{2},{3},{4},{5}\n".format(0,left_start, right_start,x_position,y_position,theta))
 Roomba.StartQueryStream(43,44)
-
+whynot = False
 while True:
 	try:
 		# Tell the roomba to move
@@ -171,14 +171,15 @@ while True:
 				break
 		Roomba.Move(0,0)
 		time.sleep(.01)
-		while True:
+		while whynot == False:
 			try:
 				x_final = float(input("x position:"))
 				y_final = float(input("y position:"))
-				break
+				whynot = True
 			except ValueError:
 				print("Please input a number")
 				continue
+		whynot = False
 	except KeyboardInterrupt:
 		break
 Roomba.Move(0,0)
