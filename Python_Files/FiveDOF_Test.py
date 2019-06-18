eaeea2c537982e879a64611c54c2a7662c9f96b3
David Croft <dcroft2001@gmail.com>
2019-06-18 16:06:07 -0400

    Update FiveDOF_Test.py

--- Python_Files/FiveDOF_Test.py
+++ Python_Files/FiveDOF_Test.py
@@ -99,7 +99,6 @@ dict = {0:[0,0,2],
 
 # Get initial wheel encoder values
 [left_start,right_start]=Roomba.Query(43,44)
-data_time_init = time.time()
 
 #Get initial IMU readings and then print them out
 accel_x, accel_y, accel_z = imu.acceleration
@@ -133,6 +132,10 @@ distance_per_count = (wheel_diameter*math.pi)/counts_per_rev
 
 start_time = time.time()
 data_time = time.time()
+data_time_init = time.time() - data_time
+
+file.write("{0:0.6f},{1:0.5f},{2:0.5f},{3:0.5f},{4:0.5f},{5:0.5f},{6:0.5f},{7:0.5f},{8:0.5f},{9:0.5f},{10},{11},{12:.6f},{13:.6f},{14:.6f}\n"\
+	.format(data_time_init, accel_x, accel_y, accel_z,mag_x, mag_y, mag_z,gyro_x, gyro_y, gyro_z, left_encoder, right_encoder, r_estimate_x, r_estimate_y, r_estimate_z))
 
 Roomba.StartQueryStream(43,44)
 
