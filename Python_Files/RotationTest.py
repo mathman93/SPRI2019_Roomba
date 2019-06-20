3c195214129fe57b9f97a9cd2cb995111e903eb0
David Croft <dcroft2001@gmail.com>
2019-06-20 14:27:11 -0400

    Update RotationTest.py

--- Python_Files/RotationTest.py
+++ Python_Files/RotationTest.py
@@ -281,10 +281,11 @@ for i in range(len(dict.keys())):
 			# Print the left encoder, right encoder, x position, y position, and theta
 			print('L/R Wheel Encoders (counts): {0},{1}'.format(left_encoder,right_encoder))
 			print('Roomba X/Y Position (mm): {0:.3f},{1:.3f}'.format(x_position,y_position))
-			print('Roomba Orientation (radians): {0:.6f}'.format(theta))
 			print('X estimate: {0:.6f}'.format(r_estimate_x))
 			print('Y estimate: {0:.6f}'.format(r_estimate_y))
 			print('Z estimate: {0:.6f}'.format(r_estimate_z))
+			print('Encoder Rotation (radians): {0:.6f}'.format(theta))
+			print('Gyroscope Rotation (radians): {0:.6f}'.format(new_theta))
 			# Write IMU data and wheel encoder data to a file.
 			#file.write("{0:0.6f},{1:0.5f},{2:0.5f},{3:0.5f},{4:0.5f},{5:0.5f},{6:0.5f},{7:0.5f},{8:0.5f},{9:0.5f},{10},{11},{12:0.5f},{13:0.5f},{14:0.5f},{15:0.5f},{16:0.5f},{17:0.5f},{18:0.5f},{19:0.5f},{20:0.5f},{21:0.5f},{22:0.5f}\n"\
 				#.format(data_time2, accel_x, accel_y, accel_z,mag_x, mag_y, mag_z,gyro_x, gyro_y, gyro_z, left_encoder, right_encoder, i_norm[0],i_norm[1],i_norm[2],j_norm[0],j_norm[1],j_norm[2],k_norm[0],k_norm[1],k_norm[2],theta,new_theta))
