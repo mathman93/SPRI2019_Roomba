4a804c4b5fef917db5e1cc7d159ff039f4b92e28
Timothy Anglea <mathman93@gmail.com>
2019-06-28 10:41:36 -0400

    changing back

--- Python_Files/Dock_Test.py
+++ Python_Files/Dock_Test.py
@@ -50,7 +50,7 @@ Roomba = RoombaCI_lib.Create_2("/dev/ttyS0", 115200)
 Roomba.ddPin = 23 # Set Roomba dd pin number
 GPIO.setup(Roomba.ddPin, GPIO.OUT, initial=GPIO.LOW)
 
-Roomba.ShutDown()
+Roomba.DirectWrite(135)
 time.sleep(5.0)
 
 print("Start OI")
