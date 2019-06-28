97cba6ed91c3aba61ea8a88acc9f2a1380a582f9
Timothy Anglea <mathman93@gmail.com>
2019-06-28 13:11:51 -0400

    trying a thing

--- Python_Files/Dock_Test.py
+++ Python_Files/Dock_Test.py
@@ -50,6 +50,8 @@ Roomba = RoombaCI_lib.Create_2("/dev/ttyS0", 115200)
 Roomba.ddPin = 23 # Set Roomba dd pin number
 GPIO.setup(Roomba.ddPin, GPIO.OUT, initial=GPIO.HIGH)
 
+Roomba.DirectWrite(7)
+time.sleep(10)
 print("Start OI")
 Roomba.DirectWrite(128) # From off, start Roomba OI (sets to Passive)
 time.sleep(1.0)
