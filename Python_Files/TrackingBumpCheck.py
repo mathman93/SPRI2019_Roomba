0e6128589f11a47f2bf527902be4d753117f62d6
David Croft <dcroft2001@gmail.com>
2019-06-28 09:42:58 -0400

    Update TrackingBumpCheck.py

--- Python_Files/TrackingBumpCheck.py
+++ Python_Files/TrackingBumpCheck.py
@@ -93,17 +93,17 @@ while True:
 			if time.time() - bump_time < backup_time: # If hasn't backed up for long enough...
 				if bump_code == 1: # If bump right...
 					f = -100
-					s = -50
+					s = -100
 				if bump_code == 2 or bump_code == 3: # If bump left or center...
 					f = -100
-					s = 50
+					s = 100
 			elif bump_mode: # If not having to back up but still has bumped into something before...
 				if bump_code == 1:
 					f = 100
-					s = 30
+					s = 15
 				if bump_code == 2 or bump_code == 3:
 					f = 100
-					s = -30
+					s = -15
 			else:
 				f = 100
 				s = 0
