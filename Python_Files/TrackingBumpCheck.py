cf0b350fd0ac35077152bc3fa93ad227ca991b59
David Croft <dcroft2001@gmail.com>
2019-06-28 10:28:49 -0400

    Update TrackingBumpCheck.py

--- Python_Files/TrackingBumpCheck.py
+++ Python_Files/TrackingBumpCheck.py
@@ -76,55 +76,55 @@ C_theta = (wheel_diameter*math.pi)/(counts_per_rev*distance_between_wheels)
 distance_per_count = (wheel_diameter*math.pi)/counts_per_rev
 data_time = time.time()
 
-bump_mode = False
+bump_mode = False # Used to tell whether or not the roomba has bumped into something and is supposed to be "tracking"
 
 #file.write("{0},{1},{2},{3},{4},{5}\n".format(0,left_start, right_start,x_position,y_position,theta))
-Roomba.StartQueryStream(7)
+Roomba.StartQueryStream(7) # Start getting bumper values
 
 while True:
 	try:
 		if Roomba.Available()>0:
 			data_time2 = time.time()
-			# Get bump value, light bumper value, then get left and right encoder values and find the change in each
+			# Get bump value
 			[bump] = Roomba.ReadQueryStream(7)
 
 			if(bump%4) > 0: # If the roomba bumps into something...
 				bump_time = time.time() #Sets up timer
 				bump_code = (bump%4) #Will tell if left/right/center bump
 				bump_mode = True # Keeps in memory that the roomba will now try and track the object it bumped into
-				bump_track += 1
-			if time.time() - bump_time < backup_time and bump_track < 2: # If hasn't backed up for long enough...
+				bump_count += 1 # Increases whenever the roomba bumps into something
+			if time.time() - bump_time < backup_time and bump_count < 2: # If hasn't backed up for long enough and it's the first bump...
 				if bump_code == 1: # If bump right...
-					f = -50
+					f = -50 #Back up, spin counterclockwise
 					s = -150
 				if bump_code == 2 or bump_code == 3: # If bump left or center...
-					f = -50
+					f = -50 #Back up, spin clockwise
 					s = 150
-			elif time.time() - bump_time < (backup_time / 2):
+			elif time.time() - bump_time < (backup_time / 2): # If hasn't backed up for long enough and is not the first bump...
 				if bump_code == 1: # If bump right...
-					f = -50
+					f = -50 # Back up, spin counterclockwise slower
 					s = -50
 				if bump_code == 2 or bump_code == 3: # If bump left or center...
-					f = -50
+					f = -50 #Back up, spin clockwise slower
 					s = 50
 			elif bump_mode and time.time() - bump_time < (backup_time + corner_time): # If not having to back up but still has bumped into something before...
-				if bump_code == 1:
-					f = 100
+				if bump_code == 1: # If bump right...
+					f = 100 # Go forward, turn clockwise
 					s = 15
-				if bump_code == 2 or bump_code == 3:
-					f = 100
+				if bump_code == 2 or bump_code == 3: # If bump left...
+					f = 100 # Go forward, turn counterclockwise
 					s = -15
-			elif bump_mode:
-				if bump_code == 1:
-					f = 100
+			elif bump_mode: # If not having to back up and bumped into something, but been a while...
+				if bump_code == 1: # If bump right...
+					f = 100 # Go forward, turn hard clockwise
 					s = 100
-				if bump_code == 2 or bump_code == 3:
-					f = 100
+				if bump_code == 2 or bump_code == 3: # If bump left...
+					f = 100 # Go forward, turn hard counterclockwise
 					s = -100
-			else:
-				f = 100
+			else: # If haven't bumped into anything left...
+				f = 100 # Go straight forward
 				s = 0
-			Roomba.Move(f,s)
+			Roomba.Move(f,s) # Move with given forward and spin values
 
 			#file.write("{0},{1},{2},{3},{4},{5}\n".format(data_time2-data_time,left_encoder, right_encoder,x_position,y_position,theta))
 	except KeyboardInterrupt:
